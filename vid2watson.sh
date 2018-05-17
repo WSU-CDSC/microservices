cc004578e436da07632d14f2766137566ab9fa6f
Weaver <weevz@uw.edu>
2018-05-17 11:34:05 -0700

    initial commit

---
+++ vid2watson.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+target="${1}"
+project_name=$(basename "${target}" | cut -d'.' -f1)
+source_dir=$(dirname "${target}")
+outdir="${source_dir}"/"${project_name}"
+audio_track="${project_name}"_audio.flac
+mkdir "${outdir}"
+cd "${outdir}"
+
+#Extract/Convert audio track to mono FLAC at 16 kHz
+ffmpeg -i "${target}" -map 0:a:0 -ac 1 -ar 16000 "${audio_track}"
+for i in *.flac ; do
+    curl -X POST -u USERNAME:PASSWORD \
+    --header "Content-Type: audio/flac" \
+    --data-binary  @"${outdir}"/"${i}" \
+    "https://stream.watsonplatform.net/speech-to-text/api/v1/recognize?timestamps=true" >> "${outdir}"/"${project_name}".json 
+done
+
+#Cleanup
+rm "${outdir}"/*.flac
+cat "${outdir}"/"${project_name}".json | grep transcript | cut -d'"' -f4 > "${outdir}"/"${project_name}"_parsed.txt
