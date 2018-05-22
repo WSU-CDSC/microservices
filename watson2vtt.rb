5f4d2d8a535a2106058f7f6ef7f63ccc4716ac82
Weaver <weevz@uw.edu>
2018-05-22 14:13:29 -0700

    start to auto-chunk segments

--- watson2vtt.rb
+++ watson2vtt.rb
@@ -10,14 +10,45 @@ ARGV.each do |input|
   File.open("#{outputpath}/#{outputfile}.vtt", 'w') do |file|
     file.puts "WEBVTT"
     file.puts ""
-    data['results'].each do |result|
-      result['alternatives'].each do |alternative|
-        intime = Time.at(alternative['timestamps'][0][1]).utc.strftime("%H:%M:%S.%s0")
-        text = alternative['transcript']
-        text.gsub! "\%HESITATION" , '(PAUSE)'
-        outtime = Time.at(alternative['timestamps'][-1][-1]).utc.strftime("%H:%M:%S.%s0")
-        file.puts "#{intime} --> #{outtime}"
-        file.puts text
+    data['results'].each do |results|
+      intime = results['alternatives'][0]['timestamps'][0][1]
+      outtime = results['alternatives'][0]['timestamps'][-1][-1]
+      segment_length = outtime - intime
+
+      if segment_length >= 7
+        results['alternatives'][0]['timestamps'].each.with_index do|timestamps, index|
+          if timestamps[1] >= intime + 7 || timestamps[1] == results['alternatives'][0]['timestamps'].last[1]
+            if ! defined? @segment_position
+              @segment_position = 0
+            else
+              @segment_position = @newsegment_position
+            end
+            @newsegment_position = index
+            @segment_outtime = (timestamps[2] - 0.5).round(2)
+
+            if timestamps[1] == results['alternatives'][0]['timestamps'].last[1]
+              segment_text = results['alternatives'][0]['timestamps'][@segment_position..(@newsegment_position)]
+              @newsegment_position = 0
+            else
+              segment_text = results['alternatives'][0]['timestamps'][@segment_position..(@newsegment_position -1)]
+            end
+            concatinated_text = Array.new
+            segment_text.each do |text|
+              concatinated_text << text[0]
+            end
+            intime_normalized = Time.at(intime).utc.strftime("%H:%M:%S.%s0")
+            outtime_normalized = Time.at(@segment_outtime).utc.strftime("%H:%M:%S.%s0")
+            file.puts "#{intime_normalized} --> #{outtime_normalized}"
+            file.puts concatinated_text.join(' ')
+            file.puts ""
+            intime = timestamps[1]
+          end
+        end
+      else
+        intime_normalized = Time.at(intime).utc.strftime("%H:%M:%S.%s0")
+        outtime_normalized = Time.at(outtime).utc.strftime("%H:%M:%S.%s0")
+        file.puts "#{intime_normalized} --> #{outtime_normalized}"
+        file.puts results['alternatives'][0]['transcript']
         file.puts ""
       end
     end
