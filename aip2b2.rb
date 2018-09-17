a46742fa9d79f00fa219b7c7e0d4972aab6bd0e0
Weaver <weevz@uw.edu>
2018-09-17 14:21:10 -0700

    bugs

--- aip2b2.rb
+++ aip2b2.rb
@@ -9,11 +9,6 @@ ARGV.options do |opts|
   opts.parse!
 end
 
-puts $dryrun
-puts 'Class is:'
-puts $dryrun.class
-exit
-
 # Set up methods
 def premisreport(actiontype,outcome)
    @premis_structure['events'] << [{'eventType':actiontype,'eventDetail':$command,'eventDateTime':Time.now,'eventOutcome':outcome}] 
@@ -40,29 +35,29 @@ if $b2path.nil?
   exit
 end
 
- ARGV.each do |input_AIP|
-   @target_path = Pathname.new(input_AIP)
-   # Test for directory
-   if ! @target_path.directory?
-     puts "Input must be a directory! Exiting.".red && exit
-   end
-   packagename = File.basename(@target_path)
-   b2_target = $b2path + '/' + packagename
-   logfile = @target_path + 'data' + 'logs' + "#{packagename}.log"
-   @premis_structure = JSON.parse(File.read(logfile))
-   if $dryrun.nil?
-    $command = 'b2 sync ' + '"' + @target_path.to_s + '" ' + '"' + b2_target + '"'
+ARGV.each do |input_AIP|
+  @target_path = Pathname.new(input_AIP)
+  # Test for directory
+  if ! @target_path.directory?
+    puts "Input must be a directory! Exiting.".red && exit
+  end
+  packagename = File.basename(@target_path)
+  b2_target = $b2path + '/' + packagename
+  logfile = @target_path + 'data' + 'logs' + "#{packagename}.log"
+  @premis_structure = JSON.parse(File.read(logfile))
+  if $dryrun.nil?
+   $command = 'b2 sync ' + '"' + @target_path.to_s + '" ' + '"' + b2_target + '"'
   else
     $command = 'b2 sync ' + $dryrun + '"' + @target_path.to_s + '" ' + '"' + b2_target + '"'
   end
 
-   if system($command)
+  if system($command)
     puts "SUCCESS!".green
     premisreport('replication','pass')
     puts @premis_structure
-    if ! if $dryrun.nil?
+    if $dryrun.nil?
       File.open("#{@target_path.to_s}/#{packagename}_#{Time.now}_premis.log",'w') {|file| file.write(@premis_structure.to_json)}
       system($command)
     end
-   end
- end
+  end
+end
