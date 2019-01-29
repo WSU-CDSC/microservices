ba272c0f032a67f86fc993f0087cd444839821fb
Weaver <weevz@uw.edu>
2019-01-29 10:50:08 -0800

    changes for paths

--- vid2watson.sh
+++ vid2watson.sh
@@ -7,15 +7,15 @@ audio_track="${project_name}"_audio.ogg
 mkdir "${outdir}"
 
 #Extract/Convert audio track to mono ogg/vorbis at 16 kHz
-ffmpeg -i "${target}" -map 0:a:0 -ac 2 -ar 16000 -c:a vorbis -strict -2 "${outdir}/${audio_track}"
+ffmpeg -i "${target}" -map 0:a:0 -ac 2 -ar 16000 -c:a vorbis -strict -2 "${outdir}/temp_audio.ogg"
 cd "${outdir}"
 for i in *.ogg ; do
     curl -X POST -u "apikey:API-KEY-HERE" \
     --header "Content-Type: audio/ogg" \
-    --data-binary  @"${outdir}"/"${i}" \
-    "https://gateway-wdc.watsonplatform.net/speech-to-text/api/v1/recognize?profanity_filter=false&timestamps=true&inactivity_timeout=120" >> "${outdir}"/"${project_name}".json
+    --data-binary  @"${i}"\
+    "https://gateway-wdc.watsonplatform.net/speech-to-text/api/v1/recognize?profanity_filter=false&timestamps=true&inactivity_timeout=120" >> "${project_name}".json
 done
 
 #Cleanup
-rm "${outdir}"/*.ogg
-cat "${outdir}"/"${project_name}".json | grep transcript | cut -d'"' -f4 > "${outdir}"/"${project_name}"_parsed.txt
+rm ./*.ogg
+cat "${project_name}".json | grep transcript | cut -d'"' -f4 > "${project_name}"_parsed.txt
