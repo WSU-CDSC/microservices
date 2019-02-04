12d29aa3277c798a7a8c257f68c744f466c84263
Weaver <weevz@uw.edu>
2019-02-04 14:45:45 -0800

    changes for escaping

--- vid2watson.rb
+++ vid2watson.rb
@@ -10,9 +10,9 @@ ARGV.each do |inputFile|
   outputOgg = "#{outputDirectory}/temp.ogg"
   outputJSON = "#{outputDirectory}/#{projectName}.json"
   Dir.mkdir(outputDirectory)
-  command = "ffmpeg -i '#{target}' -map 0:a:0 -ac 2 -ar 16000 -c:a vorbis -strict -2 '#{outputOgg}'"
+  command = 'ffmpeg -i ' + '"' + target + '"' + ' -map 0:a:0 -ac 2 -ar 16000 -c:a vorbis -strict -2 ' + '"' + outputOgg + '"'
   system(command)
-  watsonCommand = "curl -X POST -u 'apikey:KEY-GOES-HERE' --header 'Content-Type: audio/ogg' --data-binary  @'#{outputOgg}' 'https://gateway-wdc.watsonplatform.net/speech-to-text/api/v1/recognize?profanity_filter=false&timestamps=true&inactivity_timeout=120'"
+  watsonCommand = "curl -X POST -u 'apikey:KEY-GOES-HERE' --header 'Content-Type: audio/ogg' --data-binary  @" + '"' + outputOgg + '"' + " 'https://gateway-wdc.watsonplatform.net/speech-to-text/api/v1/recognize?profanity_filter=false&timestamps=true&inactivity_timeout=120'"
   puts watsonCommand
   watsonOutput = JSON.parse(`#{watsonCommand}`)
   File.open(outputJSON, 'w') do |f|
