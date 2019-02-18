795cda710fb074260913be0e4460704d3efb4e8e
Weaver <weevz@uw.edu>
2019-02-18 14:01:22 -0800

    continued changes

--- monitor-archive.rb
+++ monitor-archive.rb
@@ -9,6 +9,8 @@ watchDir = ARGV[0]
 scanDirList = Dir.glob("#{watchDir}/*")
 changedDirs = Array.new
 changedWithMeta = Array.new
+changedNoMeta = Array.new
+needExamination = Array.new
 
 scanDirList.each do |scanDir|
   logTimeRead(scanDir)
@@ -18,26 +20,40 @@ scanDirList.each do |scanDir|
 end
 
 changedDirs.each do |checkForMetadata|
-  directoryContents = Dir.glob("#{checkForMetadata}/**/*")
-  directoryContents.each do |metaCheck|
-    if File.basename(metaCheck) == 'metadata'
-      if File.mtime(metaCheck) < File.mtime(File.dirname(metaCheck))
-        changedWithMeta << File.dirname(metaCheck)
-      end
-    end
+  metaDir = "#{checkForMetadata}/metadata"
+  md5Base = File.basename(checkForMetadata) + '.md5'
+  md5File = "#{metaDir}/#{md5Base}"
+  if ! File.exist?(metaDir)
+    changedNoMeta << checkForMetadata
+  elsif ! File.exist?(md5File)
+    changedNoMeta << checkForMetadata
+  elsif File.mtime(metaDir) < File.mtime(checkForMetadata)
+    changedWithMeta << checkForMetadata
   end
 end
 
-changedNoMeta = changedDirs - changedWithMeta
-red("Directories found that do not contain metadata")
-purple("Will generate metadata")
-changedNoMeta.each do |needsMeta|
-  green("Generating metadata for: #{needsMeta}")
-  CleanUpMeta(needsMeta)
-  logTimeWrite(needsMeta)
+if ! changedNoMeta.empty?
+  red("Directories found that do not contain metadata")
+  purple("Will generate metadata")
+  changedNoMeta.each do |needsMeta|
+    green("Generating metadata for: #{needsMeta}")
+    CleanUpMeta(needsMeta)
+    logTimeWrite(needsMeta)
+  end
 end
 
-red("Directories found with inacurate existing metadata")
-changedWithMeta.each do |target|
-  CompareContents(target)
+if ! changedWithMeta.empty?
+  changedWithMeta.each do |target|
+    CompareContents(target)
+    if @missingFiles.empty? &&  @fixityCheck != 'fail'
+      logTimeWrite(target)
+    else
+      needExamination << target
+    end
+  end
+  puts "Needs Examination!"
+  puts needExamination
 end
+
+unchangedDirList = (scanDirList - changedNoMeta - changedWithMeta)
+unchangedDirList.each { |target| logTimeWrite(target) }
--- wsu-functions.rb
+++ wsu-functions.rb
@@ -5,9 +5,9 @@ require 'tempfile'
 
 # function for checking current files agains files contained in .md5 file
 def CompareContents(changedDirectory)
-  puts changedDirectory
+  puts "Changed directory found: #{changedDirectory}"
   baseName = File.basename(changedDirectory)
-  hashDataFile = Dir.glob("#{changedDirectory}/**/#{baseName}.md5")[0]
+  hashDataFile = "#{changedDirectory}/metadata/#{baseName}.md5"
   allFiles = Dir.glob("#{changedDirectory}/**/*").reject {|f| File.directory?(f)}
   hashData = File.readlines(hashDataFile)
   hashFileList = Array.new
@@ -36,13 +36,42 @@ def CompareContents(changedDirectory)
     puts "No file discrepencies found"
   else
     @newFiles = (currentFileList - hashFileList.uniq)
-    if ! @newFiles.empty?
-      red("New Files Found!")
+    if ! @newFiles.empty? && @missingFiles.nil?
+      red("New Files Found in #{changedDirectory}!")
+      purple("Will verify hashes for existing files")
+      verifyExistingHashManifest(changedDirectory)
+      if @fixityCheck == 'pass'
+        green("Existing hashes for #{changedDirectory} were valid: Will generate new metadata to reflect new files.")
+        CleanUpMeta(changedDirectory)
+      elsif
+        @fixityCheck == 'fail'
+        red("Warning: Invalid hash information detected. Please examine #{changedDirectory} for changes")
+      end
     end
     @missingFiles = (hashFileList.uniq - currentFileList)
     if ! @missingFiles.empty?
-      red("Missing Files Found!")
+      red("Warning! Missing files found in #{changedDirectory}!")
     end
+  end
+end
+
+def verifyExistingHashManifest(fileInput)
+  sorted_hashes = Tempfile.new
+  targetDir = File.expand_path(fileInput)
+  baseName = File.basename(targetDir)
+  hashMeta = "#{targetDir}/metadata/#{baseName}.md5"
+  manifest = File.readlines(hashMeta)
+  manifest.uniq.each do |line|
+    sorted_hashes << line
+  end
+  sorted_hashes.rewind
+  command = "hashdeep -k '#{sorted_hashes.path}' -xrle '#{fileInput}'"
+  if system(command)
+    puts "Fixity infomation valid"
+    @fixityCheck = 'pass'
+  else
+    puts "Bad fixity information present!"
+    @fixityCheck = 'fail'
   end
 end
 
@@ -74,7 +103,7 @@ def makeHashdeepMeta(targetDir,hashMeta)
   Dir.chdir(targetDir)
   hashDeepCommand = "hashdeep -c md5 -r -l ./"
   hashDeepOutput = `#{hashDeepCommand}`
-  File.write(hashMeta,"'" + hashDeepOutput + "'")
+  File.write(hashMeta,hashDeepOutput)
 end
 
 # makes an exiftool sidecar in JSON
