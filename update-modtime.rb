cccf66d797bcc099380d03751975b33e80b48300
Weaver <weevz@uw.edu>
2019-04-15 14:04:10 -0700

    change to better wording

--- update-modtime.rb
+++ update-modtime.rb
@@ -2,9 +2,9 @@
 scriptLocation = File.expand_path(File.dirname(__FILE__))
 require "#{scriptLocation}/wsu-functions.rb"
 
-inputDirs = ARGV
+inputDirs = ARGV.select { |dir| File.directory?(dir) }
 inputDirs.each do |targetDir|
-  green("Updating metadata and log time for #{targetDir}")
+  green("Generating metadata and updating log time for #{targetDir}")
   CleanUpMeta(targetDir)
   logTimeWrite(targetDir)
 end
