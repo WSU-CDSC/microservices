a212e8ae4f302eb2370d725a8aff704ceb4292b4
Andrew Weaver <weevz@uw.edu>
2019-04-16 16:37:56 -0700

    progress bar and start csv

--- ocr_test.rb
+++ ocr_test.rb
@@ -4,6 +4,7 @@ require 'pdf-reader'
 
 # Find PDF Inputs
 pdf_files = []
+csv_out = []
 ARGV.each do |target|
   if File.directory?(target)
     files = Dir.glob("#{target}/**/*.pdf")
@@ -15,11 +16,11 @@ ARGV.each do |target|
   end
 end
 
+progress_count = 1
 pdf_files.each do |file|
   pdf_text = 0
   pdf_data = PDF::Reader.new(file)
   if pdf_data.page(1).text.length == 0
-    puts "ZERO"
     pdf_data.pages.each do |page|
       pdf_text += page.text.length
     end
@@ -27,8 +28,12 @@ pdf_files.each do |file|
     pdf_text += pdf_data.page(1).text.length
   end
   if pdf_text > 5
-    puts "TEXT FOUND in #{file}!"
+    csv_out << [file,'Text Detected']
   else
-    puts "NO TEXT found in #{file}!"
+    csv_out << [file,'No Text Detected']
   end
+  printf("\rChecking file %d of #{pdf_files.count}",progress_count)
+  progress_count += 1
 end
+
+puts csv_out
