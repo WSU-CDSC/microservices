74584822232446085978c5e65f5dad22ea80dfee
Weaver <weevz@uw.edu>
2019-04-17 11:09:51 -0700

    error checking and csv

--- ocr_test.rb
+++ ocr_test.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 
 require 'pdf-reader'
+require 'csv'
 
 # Find PDF Inputs
 pdf_files = []
@@ -16,24 +17,41 @@ ARGV.each do |target|
   end
 end
 
+at_exit do
+  unless csv_out.empty?
+    timestamp = Time.now.strftime('%Y-%m-%d_%H-%M-%S')
+    output_csv = ENV['HOME'] + "/Desktop/ocr-scan_#{timestamp}.csv"
+    CSV.open(output_csv, 'wb') do |csv|
+      headers = ['Filename', 'Text']
+      csv << headers
+      csv_out.each { |line| csv << line }
+    end
+  end
+end
+
 progress_count = 1
 pdf_files.each do |file|
-  pdf_text = 0
-  pdf_data = PDF::Reader.new(file)
-  if pdf_data.page(1).text.length == 0
-    pdf_data.pages.each do |page|
-      pdf_text += page.text.length
+  begin
+    pdf_text = 0
+    pdf_data = PDF::Reader.new(file)
+    if pdf_data.page(1).text.length == 0
+      pdf_data.pages.each do |page|
+        pdf_text += page.text.length
+      end
+    else
+      pdf_text += pdf_data.page(1).text.length
     end
-  else
-    pdf_text += pdf_data.page(1).text.length
-  end
-  if pdf_text > 5
-    csv_out << [file,'Text Detected']
-  else
-    csv_out << [file,'No Text Detected']
+    if pdf_text > 5
+      csv_out << [file,'Text Detected']
+    else
+      csv_out << [file,'No Text Detected']
+    end
+  rescue
+    csv_out << [file,'FILE SCAN ERROR']
+    printf("\rChecking file %d of #{pdf_files.count}",progress_count)
+    progress_count += 1
+    next
   end
   printf("\rChecking file %d of #{pdf_files.count}",progress_count)
   progress_count += 1
 end
-
-puts csv_out
