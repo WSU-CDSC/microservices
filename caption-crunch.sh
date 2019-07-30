0e88daf8c0ac20ffe31f364858fb979be5b0525c
Weaver <weevz@uw.edu>
2019-07-30 15:55:32 -0700

    fix errors

--- caption-crunch.sh
+++ caption-crunch.sh
@@ -3,21 +3,22 @@
 # Input needs to be a file containing a list of download links
 
 targetList="${1}"
+script_dir=$(dirname "$0")
 outputDir="$(dirname "${targetList}")"
 cd "${outputDir}"
 echo "Downloading Targets: Please wait!"
-cat targets.txt | parallel youtube-dl {}
+cat "${targetList}" | parallel youtube-dl {}
 echo "Sending files to Watson: Please wait!"
-find . -iname "*.mp4" -o -iname "*.mkv" -o -iname "*.m4a" | parallel vid2watson.rb {}
+find . -iname "*.mp4" -o -iname "*.mkv" -o -iname "*.m4a" | parallel ruby "${script_dir}/vid2watson.rb" "{}"
 if ! [ -d JSON ] ; then
     mkdir JSON
 fi
 if ! [ -d VTT ] ; then
     mkdir VTT
 fi
-echo "Moving generating VTT and moving outputs: Please wait!"
+echo "Generating VTT and moving outputs: Please wait!"
 find . -iname "*.json" | while read JSON ; do
-    watson2vtt.rb "${JSON}"
+    ruby "${script_dir}/vid2watson.rb" "${JSON}"
     cp "${JSON}" JSON
 done
 
